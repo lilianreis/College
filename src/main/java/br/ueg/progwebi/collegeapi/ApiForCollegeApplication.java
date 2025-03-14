87c783c1a2e9ce64ecae11a2682e96a5830b369f
Lilian Reis <lilianreisbp@gmail.com>
2025-03-14 20:51:11 -0300

    versao inicial

---
+++ src/main/java/br/ueg/progwebi/collegeapi/ApiForCollegeApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.progwebi.collegeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForCollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForCollegeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/progwebi/collegeapi/ApiForCollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.progwebi.collegeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForCollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
